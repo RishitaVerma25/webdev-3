9edf302007feb0ea54970bcba2f15d55d27487b1
RishitaVerma25 <rishitaverma821@gmail.com>
2026-09-05 22:05:06 +0530

    DAY2

--- app.js
+++ app.js
@@ -35,3 +35,31 @@
 
 require("dotenv").config();
 console.log(process.env.PORT)
+
+// const http = require("http");
+// const server = http.createServer((req, res)=>{
+//     res.write("<h1>Welcome to node.js</h1>");
+//     res.end();
+// });
+
+// // run the server
+// server.listen(process.env.PORT || 5000, () => {
+//     console.log(`Running on port ${process.env.PORT}`)
+// });
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+        <h1 style='color:navy'>Hello from Node.js!</h1>
+        <p>Built with pure Node.js http module.</p>
+    `);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Route not found' }));
+  }
+});
+
+server.listen(3000, () => console.log('Running on :3000'));
